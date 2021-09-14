702178985af0f1c29719740997e2e7d598b3a7c5
IlyaGitHub <ilya-levoshko@mail.ru>
2021-09-14 13:05:17 +0300

    Updated RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $role = Role::where('name', 'User')->findOrFail();
+        $role = Role::where('name', 'User')->firstOrFail();
 
         return User::create([
             'name' => $data['name'],
